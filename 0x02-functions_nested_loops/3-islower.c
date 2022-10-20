d3ba9988c95fc5840dfe623a1b725528b6dbabef
Xeekay <zachariah0019@gmail.com>
2022-10-19 17:38:15 -0700

    sleep

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - cheaks
+ * @c: the character
+ * Return: 1 if letter is lower, 0 if not
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
