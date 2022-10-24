501dbad49fe608e28bf622df30bc52c4bfdb6e34
Xeekay <zachariah0019@gmail.com>
2022-10-24 15:07:22 -0700

    b3

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,11 @@
 /**
- *
  *  *_strlen-Finds the length of a string.
  *
  *   *@s:String pointer to the string whose length is to be found.
  *
  *    *Return: returns the length of the string.
  *
- *     */
+ */
 
 
 
