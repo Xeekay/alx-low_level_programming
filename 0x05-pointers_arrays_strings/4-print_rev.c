c72d3b843c703c779d1970a0adabe4fa97b780ad
Xeekay <zachariah0019@gmail.com>
2022-10-24 17:05:04 -0700

    b5

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,17 +8,17 @@
  */
 void print_rev(char *s)
 {
-	 int count = 0;
+	int count = 0;
 
- 	 while (count >= 0)
-	 {
-		 if (s[count] == '\0')
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
 			 
-			 break;
-		 count++;
-	 }
+			break;
+		count++;
+	}
 	 
-	 for (count--; count >= 0; count--)
-		 _putchar(s[count]);
-	 _putchar('\n');
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
+	_putchar('\n');
 }
